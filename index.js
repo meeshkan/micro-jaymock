5d0c5d25f777e507fb2f61b83d2d5e8465ac579d
k4m4 <nikolaskam@gmail.com>
2019-11-13 18:33:45 -0500

    Change main file to index.ts

--- index.js
+++
@@ -1,7 +0,0 @@
-const jaymock = require('@unmock/jaymock')
-const { json } = require('micro')
-
-const jm = jaymock()
-jm.extend('chance', new require('chance')()) // Add custom functions using `jaymock.extend()`
-
-module.exports = async (req, res) => jm.populate(await json(req))
