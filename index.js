e1c94b2bc944d66ca9a37598243b2dc924d30332
k4m4 <nikolaskam@gmail.com>
2019-11-13 16:31:54 -0500

    Expose microservice

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const { json } = require('micro')
 
 const jm = jaymock()
 jm.extend('chance', new require('chance')()) // Add custom functions using `jaymock.extend()`
+
+module.exports = async (req, res) => jm.populate(await json(req))
