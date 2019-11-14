a53912eab111fcc9528a57befa06ee40b9de9e38
k4m4 <nikolaskam@gmail.com>
2019-11-13 19:23:06 -0500

    Serve readme on GET request to "/"

--- index.ts
+++ index.ts
@@ -1,13 +1,36 @@
 import { IncomingMessage, ServerResponse } from 'http'
 const { json, send } = require('micro')
-
 const jaymock = require('@unmock/jaymock')
 const chance = require('chance')
+const serveMarked = require('serve-marked')
+const fs = require('fs')
 
 const jm = jaymock()
 jm.extend('chance', new chance())
 
+const serveReadme = serveMarked(fs.readFileSync('./README.md', 'utf8'), {
+    title: 'jaymock',
+    inlineCSS: `
+        .markdown-body h1 + p {
+            text-align: center;
+            margin: -40px 0 4rem 0;
+            line-height: 20px;
+            height: 20px;
+        }
+    `
+})
+
 module.exports = async (req: IncomingMessage, res: ServerResponse) => {
+    if (req.method === 'GET') {
+        switch (req.url) {
+            case '/':
+                return serveReadme(req, res)
+            case '/favicon.ico':
+                return ''
+            default:
+                return 'Use POST request.'
+        }
+    }
     try {
         send(res, 200, jm.populate(await json(req)))
     } catch (e) {
